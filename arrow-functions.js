891051e524a79d37b8c9742b7ace4d2f2ea88f94
TimRas <martinusras95@gmail.com>
2022-05-16 22:30:10 +0000

    add basic arrow functions matter

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,47 @@
 
 // Vanilla JavaScript Function
 
-// Arrow Function With Parameters
+    function addTwoNumber(a, b) {
+        // code block
+        return a + b;
+    }
 
-// Single Line Arrow Function With Parameters
+    let sum = addTwoNumber(3, 5);
+    console.log(sum);
+
+// // Arrow Function With Parameters
+
+    const addTwoNumber = (a, b) => {
+        // code block
+        return a + b;
+    }
+
+    let sum3 = addTwoNumber(3, 5);
+    console.log(sum3);
+
+// // Single Line Arrow Function With Parameters
+
+    const addTwoNumbers = (a, b) => a + b;
+    const addTwoNumbers2 = (a, b) => (a + b); //* also valid *//
+    
+    let sum2 = addTwoNumbers (4, 8);
+    console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("dikke bana");
+
+const sayHello = () => console.log('hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const multipleLinesOfCode = () => (
+    `<p>
+        Hallo allemaal,
+        dit is het dan.
+    </p>`
+)
+
+console.log(multipleLinesOfCode());
